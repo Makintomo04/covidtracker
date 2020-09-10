7204d6b09c3eda96dad20220b6b03eb10a0728f6
Michael Akintomo <1721809@brunel.ac.uk>
2020-09-11 00:28:29 +0100

    material ui import + header skeleton implemented.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Covid 19 Tracker</h1>
+    <div className="app">
+      <div className="app__header">
+        <h1>CORONAVIRUS STATISTICS</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="Value">
+            <MenuItem value="worldwide">Country 1 </MenuItem>
+            <MenuItem value="worldwide">Country 2 </MenuItem>
+            <MenuItem value="worldwide">Country 3 </MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
